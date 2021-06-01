6f3df452d5239b3cbf682a393f6c6227563ebbce
Renzonius <renzo.mac@hotmail.com>
2021-06-01 20:41:14 -0300

    Offline 4

--- sw.js
+++ sw.js
@@ -115,25 +115,17 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-  // console.log('[Service Worker] Fetch', event.request.url);
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResponse = await event.preloadResponse;
-        if (preloadResponse) {
-          return preloadResponse;
-        }
-
-        const networkResponse = await fetch(event.request);
-        return networkResponse;
-      } catch (error) {
-        console.log('[Service Worker] Fetch failed; returning offline page instead.', error);
-
-        const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(OFFLINE_URL);
-        return cachedResponse;
-      }
-    })());
-  }
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((r) => {
+          console.log('[Servicio Worker] Obteniendo recurso: '+event.request.url);
+      return r || fetch(event.request).then((response) => {
+                return caches.open(CACHE_NAME).then((cache) => {
+          console.log('[Servicio Worker] Almacena el nuevo recurso: '+event.request.url);
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
 });
